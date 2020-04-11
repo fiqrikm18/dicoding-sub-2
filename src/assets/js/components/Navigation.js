882146b0ef2451426647e03d030b95ba54f5881b
fiqri khoirul m <fiqrikm18@gmail.com>
2020-04-11 18:07:09 +0700

    add content on dashboard

--- src/assets/js/components/Navigation.js
+++ src/assets/js/components/Navigation.js
@@ -13,81 +13,86 @@ class Navigation extends HTMLElement {
 
     render() {
         this._shadowRoot.innerHTML = `
-        <style>
-            * {
-                margin: 0;
-                padding: 0px;
-            }
+            <style>
+                * {
+                    margin: 0;
+                    padding: 0px;
+                }
 
-            nav {
-                padding: 15px;
-                background: #4D5359;
-                display: flex;
-                flex-direction: column;
-                width: 40px;
-                min-height: 100vh;
-            }
+                nav {
+                    padding: 15px;
+                    background: #4D5359;
+                    display: flex;
+                    flex-direction: column;
+                    width: 40px;
+                    min-height: 100vh;
+                }
 
-            nav a {
-                display: flex;
-                flex-direction: column;
-                color: white;
-                text-decoration: none;
-                margin: 10px auto;
-                justify-content: center;
-            }
+                nav a {
+                    display: flex;
+                    flex-direction: column;
+                    color: white;
+                    text-decoration: none;
+                    margin: 10px auto;
+                    justify-content: center;
+                }
 
-            nav a > * {
-                margin: 3px auto;
-            }
+                nav a > * {
+                    margin: 3px auto;
+                }
 
-            nav a p {
-                font-size: 10pt;
-            }
+                nav a p {
+                    font-size: 10pt;
+                }
 
-            nav a:hover {
-                color: #79C99E;
-            }
+                nav a:hover {
+                    opacity: 0.7;
+                }
 
-            nav a img {
-                fill: #79C99E;
-            }
+                nav a img {
+                    fill: #79C99E;
+                }
 
-            .logo {
-                text-align: center;
-                color: white;
-                background: #79C99E;
-                padding: 5px;
-                font-size: 1vw;
-                border-radius: 3px;
-                margin-bottom: 10px;
-                cursor: pointer;
-            }
+                .logo {
+                    text-align: center;
+                    color: white;
+                    background: #79C99E;
+                    padding: 5px;
+                    font-size: 1vw;
+                    border-radius: 3px;
+                    margin-bottom: 10px;
+                    cursor: pointer;
+                }
 
-            .logo a {
-                margin: 0px;
-                cursor: pointer;
-            }
+                .logo a {
+                    margin: 0px;
+                    cursor: pointer;
+                    display: flex;
+                    flex-direction: column;
+                }
 
-            .logo a:hover {
-                margin: 0px;
-                color: white;
-                cursor: pointer;
-            }
-        </style>
-        <nav>
-            <div class="logo">
-                <a href="/"><p>C-19</p></a>
-            </div>
-            <a href="#">
-                <img src="${Dashboard}" alt="dashboard-logo"> 
-                <p>Home</p>
-            </a>
-            <a href="#">
-                <img src="${Report}" alt="report-logo"> 
-                <p>Report</p>
-            </a>
-        </nav>
+                .logo a:hover {
+                    margin: 0px;
+                    color: white;
+                    cursor: pointer;
+                }
+            </style>
+            <nav>
+                <div class="logo">
+                    <a href="/">
+                    <p>C-19</p>
+                    <p style="font-size: 8pt;">Report</p>
+                    </a>
+                </div>
+                <a href="#">
+                    <img src="${Dashboard}" alt="dashboard-logo"> 
+                    <p>Home</p>
+                </a>
+                <a href="#">
+                    <img src="${Report}" alt="report-logo"> 
+                    <p>Report</p>
+                </a>
+            </nav>
         `;
     }
 }
--- src/assets/js/views/ContentContainer.js
+++ src/assets/js/views/ContentContainer.js
@@ -1,18 +1,61 @@
+import Axios from "axios";
+import config from "../confiig";
+import moment from "moment";
+
 class ContentContainer extends HTMLElement {
-    constructor() {
-        super();
+    getData() {
+        return Axios.get(config.BASE_URL)
+            .then((result) => {
+                let confirm = result.data.confirmed;
+                let recover = result.data.recovered;
+                let death = result.data.deaths;
+                let lastUpdate = result.data.lastUpdate;
+
+                this.render(confirm, recover, death, lastUpdate);
+            })
+            .catch((err) => console.log(err));
     }
 
     connectedCallback() {
-        this.render();
+        this.getData();
     }
 
-    render() {
-        this.innerHTML = `            
-            <div>
-            asd
+    render(confirm, recover, death, lstUpdate) {
+        // TODO: Create chart here
+        this.innerHTML = `
+        <div>
+            <div class="dashboard">
+                <h4>Corona Virus Report</h4>
+                <div class="card dashboard-content">
+                    <div>
+                        <div class="dashboard-items">
+                            <p>confirmed</p>
+                            <p>${confirm.value}</p>
+                        </div>
+
+                        <div class="dashboard-items">
+                            <p>recovered</p>
+                            <p>${recover.value}</p>
+                        </div>
+
+                        <div class="dashboard-items">
+                            <p>deaths</p>
+                            <p>${death.value}</p>
+                        </div>
+                    </div>
+
+                    <p class="update-status">Last Update: 
+                        <b>
+                        ${moment(lstUpdate)
+                            .utc()
+                            .format("DD MMMM YYYY, HH:mm:ss")}
+                        </b>
+                    </p>
+                </div>
+                
             </div>
-        `;
+        </div>
+    `;
     }
 }
 
